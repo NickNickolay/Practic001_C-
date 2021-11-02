68f4fedb3816201a161ac7fc427ace4b3f18f8e7
Nick <nicknickspb@icloud.com>
2021-11-02 03:08:19 +0300

    Practic002

---
+++ Practic002_C#/Program.cs
@@ -0,0 +1,56 @@
+﻿/*Имеется числовой массив A заполненный числами из отрезка [minValue; maxValue].
+ Создать на его основе масив B, отбрасывая те, которые нарушают порядок возрастания
+элементы, больше 8
+знакочередования*/
+
+int[] A = new int[10];
+int i=0;
+while(i<A.Length)
+
+{
+    A[i] = new Random().Next(-6, 31);
+    Console.Write(A[i] + " ");
+    i++;
+}
+
+Console.WriteLine();
+
+int current = A[0];
+Console.Write(current + " ");
+for(i = 1; i <A.Length; i++)
+{
+    if(A[i]> current)
+    { 
+        Console.Write(A[i] + " ");
+        current = A[i];
+        i++;
+    }
+}
+Console.WriteLine();
+/* исключаем элементы больше 8
+знакочередования*/
+
+for(i=0;i<A.Length;i++)
+{
+    if(A[i]<8)
+    Console.Write(A[i] + " ");
+}
+Console.WriteLine();
+
+/*нарушают порядок возрастания
+элементы, больше 8
+знакочередования*/
+int num = A[0];
+i = 1;
+Console.Write(num + " ");
+while(i < A.Length)
+{
+    if((num < 0 && A[i] > 0 ) || (num > 0 && A[i] < 0))
+    {
+        Console.Write(A[i] + " ");
+        num = A[i];
+        
+    }
+    i++;
+}
+
